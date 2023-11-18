3d228b634c40deb3299520b771c080453f6ff066
Maoxx <Maoxx@Git.com>
2023-11-18 17:57:50 +0800

    first commit

---
+++ git-demo01/src/main/java/com/mnkj/git/gitTest.java
@@ -0,0 +1,8 @@
+package main.java.com.mnkj.git;
+
+public class gitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+        System.out.println("aaaaaaaaaa");
+    }
+}
